1d6902b5ce52ccbfd1ab2f0ac0e35020aeca4bbb
Hector Polanco <hdp2112@columbia.edu>
2019-10-12 23:45:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
+  p array.index(value_to_find)
   return array.index(value_to_find)
 end
 
 def find_max_value(array)
+  p array.max
   return array.max
 end
 
 def find_min_value(array)
+  p array.min
   return array.min
 end
